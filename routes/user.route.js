7356ddc9136168a2ca8a9fd488eacbb7c624f0f3
Vaishnavi Phirkoj <vaishnaviphirkoj27@gmail.com>
2022-03-10 19:12:53 +0530

    updates

---
+++ routes/user.route.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+const authorization = require("../middlewares/auth.middlware");
+
+router.post(
+  "/userUnfollow",
+  authorization.authorization,
+  userController.unfollow
+);
+
+router.post("/userFollow", authorization.authorization, userController.follow);
+
+router.get("/userDetail", authorization.authorization, userController.getUser);
+
+router.post(
+  "/userDetailUpdate",
+  authorization.authorization,
+  authorization.uploadImg.single("profilepic"),
+  userController.updateUser
+);
+
+router.post("/userAdd", userController.addUser);
+
+router.post("/userLogin", userController.loginUser);
+
+module.exports = router;
