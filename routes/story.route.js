659d14cdccd2e732fc375c4b3281e03d2e77e5a6
Vaishnavi Phirkoj <vaishnaviphirkoj27@gmail.com>
2022-03-11 19:29:22 +0530

    updates

---
+++ routes/story.route.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const storyController = require("../controllers/story.controller");
+const authorization = require("../middlewares/auth.middlware");
+
+router.post(
+  "/storyAdd",
+  authorization.authorization,
+  authorization.uploadStory.single("story"),
+  storyController.addStory
+);
+
+router.get(
+  "/storiesGet",
+  authorization.authorization,
+  storyController.getStory
+);
+
+router.post(
+  "/storyView",
+  authorization.authorization,
+  storyController.viewStory
+);
+
+router.post(
+  "/storyDelete",
+  authorization.authorization,
+  storyController.deleteStory
+);
+
+module.exports = router;
